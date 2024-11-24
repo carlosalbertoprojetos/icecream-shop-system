7dd9f61076073fddd11dd5bd6dfe0646569f6741
carlos_alberto <carlosalbertoprojetos2020@gmail.com>
2024-11-24 12:27:14 -0300

    número de itens permanece no carrinho após refresh

--- apps/pedido/urls.py
+++ apps/pedido/urls.py
@@ -6,6 +6,7 @@ app_name = "pedido"
 urlpatterns = [
     path("adicionar-item/<int:item_id>/", v.adicionar_item, name="adicionar_item"),
     path("remover-item/<int:item_id>/", v.remover_item, name="remover_item"),
+    path("sacola/", v.exibir_sacola, name="exibir_sacola"),
     path("sacola-itens/", v.total_itens_sacola, name="total_itens_sacola"),
     path("checkout/", v.checkout, name="checkout_pedido"),
 ]
--- apps/pedido/views.py
+++ apps/pedido/views.py
@@ -54,6 +54,22 @@ def adicionar_item(request, item_id):
     return response
 
 
+def exibir_sacola(request):
+    sacola = get_sacola(request)
+    sacola, total_acumulado = calcular_totais(sacola)  # Calcular os totais
+    total_itens = get_total_itens(sacola)
+
+    return render(
+        request,
+        "index.html",
+        {
+            "sacola": sacola,
+            "total_itens": total_itens,
+            "total_acumulado": total_acumulado,
+        },
+    )
+
+
 def total_itens_sacola(request):
     sacola = get_sacola(request)
     total_itens = get_total_itens(sacola)
